864b00d917b17bb60ad827105c08c00b77c94654
mohamed badaoui <socialnetworks@badaoui.fr>
2021-04-04 16:42:07 +0200

    Delete asus-m433ia-numpad-driver directory

--- asus-m433ia-numpad-driver/install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo modprobe i2c-dev
-sudo cp ./M433IA_touchpad_numpad.py /usr/bin
-sudo cp ./M433IA_touchpad_numpad.service /lib/systemd/system/
-echo "i2c-dev" | sudo tee /etc/modules-load.d/i2c-dev.conf
-sudo systemctl enable M433IA_touchpad_numpad
-sudo systemctl start M433IA_touchpad_numpad
