7f9845bf82d8671b16f469aef69743a4d14c3fa4
Yannik <yannik@yannik-sc.de>
2021-06-22 18:32:34 +0200

    Add script which uses more dynamic layouts instead of hardcoded ones

---
+++ numpad_layouts/ux581l.py
@@ -0,0 +1,12 @@
+from libevdev import EV_KEY
+
+cols = 4 # As the UX581L has a "vertical numpad"
+rows = 5 # Compared to the more "horizontal" version of the UM433DA (for example)
+
+keys = [
+    [EV_KEY.KEY_KPEQUAL, EV_KEY.KEY_5, EV_KEY.KEY_BACKSPACE, EV_KEY.KEY_BACKSPACE],
+    [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPSLASH],
+    [EV_KEY.KEY_KP4, EV_KEY.KEY_KP5, EV_KEY.KEY_KP6, EV_KEY.KEY_KPASTERISK],
+    [EV_KEY.KEY_KP1, EV_KEY.KEY_KP2, EV_KEY.KEY_KP3, EV_KEY.KEY_KPMINUS],
+    [EV_KEY.KEY_KP0, EV_KEY.KEY_KPDOT, EV_KEY.KEY_KPENTER, EV_KEY.KEY_KPPLUS]
+]
