6a41ec64b2eb4bb4bd32a94cbf43d96e77ef5c97
kawaegle <alecromski>
2021-07-22 09:58:24 +0200

    try to close issue

--- asus_touchpad.py
+++ asus_touchpad.py
@@ -154,14 +154,14 @@ numlock=False
 # Process events while running #
 while True:
 
-    # If keyboard sends numlock event with F8 key tap, enable/disable touchpad events #
-    for e in d_k.events():
-        if e.matches(EV_KEY.KEY_F8) and e.value == 1:
-            numlock = not numlock
-            if numlock:
-                activate_numlock()
-            else:
-                deactivate_numlock()
+#    # If keyboard sends numlock event with F8 key tap, enable/disable touchpad events #
+#    for e in d_k.events():
+#        if e.matches(EV_KEY.KEY_F8) and e.value == 1:
+#            numlock = not numlock
+#            if numlock:
+#                activate_numlock()
+#            else:
+#                deactivate_numlock()
 
     # If touchpad sends tap events, convert x/y position to numlock key and send it #
     for e in d_t.events():
