ac374b49b37b1e692eb6bcf5e8dc8a0e21427d40
Lutz Bender <lutz@lutz-bender.de>
2022-05-23 07:32:39 +0200

    do not exit on partial uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -11,43 +11,42 @@ modprobe -r i2c-dev
 if [[ $? != 0 ]]
 then
 	echo "i2c-dev module cannot be removed successfuly..."
-	exit 1
+	# exit 1
 fi
 
 systemctl stop asus_touchpad_numpad
 if [[ $? != 0 ]]
 then
 	echo "asus_touchpad_numpad.service cannot be stopped correctly..."
-	exit 1
+	# exit 1
 fi
 
 systemctl disable asus_touchpad_numpad
 if [[ $? != 0 ]]
 then
 	echo "asus_touchpad_numpad.service cannot be disabled correctly..."
-	exit 1
+	# exit 1
 fi
 
 rm -f /etc/systemd/system/asus_touchpad_numpad.service
 if [[ $? != 0 ]]
 then
 	echo "/etc/systemd/system/asus_touchpad_numpad.service cannot be removed correctly..."
-	exit 1
+	# exit 1
 fi
 
 rm -rf /usr/share/asus_touchpad_numpad-driver/
 if [[ $? != 0 ]]
 then
 	echo "/usr/share/asus_touchpad_numpad-driver/ cannot be removed correctly..."
-	exit 1
+	# exit 1
 fi
 
 rm -rf /var/log/asus_touchpad_numpad-driver
 if [[ $? != 0 ]]
 then
 	echo "/var/log/asus_touchpad_numpad-driver cannot be removed correctly..."
-	exit 1
+	# exit 1
 fi
 
 echo "Asus touchpad python driver uninstalled"
-exit 0
